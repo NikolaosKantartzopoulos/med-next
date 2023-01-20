133eec7f848a17db06c5d35c4c10dbfabebeb976
NikolaosKantartzopoulos <nkantartzopoulos@gmail.com>
2023-01-20 07:11:35 +0200

    v1.0.2

--- pages/admin/manage-assets.js
+++ pages/admin/manage-assets.js
@@ -39,15 +39,6 @@ function ManageAssets() {
 export async function getServerSideProps(context) {
 	const session = await getSession({ req: context.req });
 
-	if (!session) {
-		return {
-			redirect: {
-				destination: "/users/browse-exams",
-				permanent: false,
-			},
-		};
-	}
-
 	return {
 		redirect: {
 			destination: "/admin/exams-table",
